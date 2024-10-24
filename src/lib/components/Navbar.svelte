83584b4eb900a36a7964da85514732d9a4f388c2
Kévin Calmant <kevin.calmant@outlook.com>
2024-10-24 22:47:22 +0200

    feat: header + some navigation styling

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+  import { onMount } from 'svelte'
+  import Navlink from './Navlink.svelte'
+
+  let currentPath: string
+
+  onMount(() => (currentPath = window.location.pathname))
+</script>
+
+<header class="col-span-3 bg-daisyBush text-center">
+  <div>
+    <a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
+  </div>
+
+  <nav>
+    <ul class="list-none text-2xl font-bold">
+      <li><Navlink href="/invoices">Invoice</Navlink></li>
+      <li><Navlink href="/clients">Clients</Navlink></li>
+      <li><Navlink href="#">Setting</Navlink></li>
+      <li><Navlink href="#">Logout</Navlink></li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="postcss">
+  nav ul li {
+    @apply mb-6;
+  }
+</style>
---
+++ src/lib/components/Navlink.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  export let href: string
+</script>
+
+<a {href} class:active={$page.url.pathname === href}><slot /></a>
+
+<style lang="postcss" scoped>
+  a {
+    @apply font-bold text-white hover:text-goldenFizz;
+  }
+
+  a.active {
+    @apply px-8 text-robinEggBlue transition-[padding];
+    background:
+      url('/images/active-nav--left.svg') left no-repeat,
+      url('/images/active-nav--right.svg') right no-repeat;
+  }
+
+  a.active:hover {
+    @apply px-9;
+  }
+</style>
