b1393ef75320e554271a65d98d5677b9440b90f6
Kévin Calmant <kevin.calmant@outlook.com>
2024-10-24 21:52:30 +0200

    tool: more tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sansSerif: ['Source Sans Pro', 'sans-serif'],
+      mono: ['Source Code Pro', 'monospace'],
+      handwriting: ['Kalam', 'cursive'],
+    },
+    extend: {
+      colors: {
+        blueGem: '#3813a0',
+        caribbeanGreen: '#00ca9e',
+        caribbeanGreen: '#1CC6A0',
+        daisyBush: '#4714a5',
+        fog: '#decaec',
+        gallery: '#efeaf2',
+        goldenFizz: '#feff40',
+        lavenderIndigo: '#8657e1',
+        monsoon: '#777878',
+        pastelPurple: '#b2a1bb',
+        prim: '#ede6ef',
+        purple: '#7209b7',
+        robinEggBlue: '#00e9c0',
+        scarlet: '#f72f45',
+        silver: '#c0c0c0',
+        whisper: '#f9f4f9',
+      },
+    },
   },
   plugins: [],
 }
