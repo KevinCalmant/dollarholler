256e40f2c7bfda602a53a43c083b174aaa37cde6
Kévin Calmant <kevin.calmant@outlook.com>
2024-10-25 09:28:09 +0200

    feat: responsible dashboard view

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -2,7 +2,7 @@
   import { Icon, MagnifyingGlass } from 'svelte-hero-icons'
 </script>
 
-<div class="flex">
+<div class="flex w-full">
   <div class="mr-2 mt-auto text-pastelPurple">
     <Icon src={MagnifyingGlass} size="28" class="text-grey hover:text-purple" />
   </div>
@@ -10,6 +10,7 @@
     type="text"
     name="search"
     placeholder="Search by keyword"
-    class="borber-b-pastelPurple w-72 border-b-2 border-dashed bg-transparent font-sansSerif text-xl text-black outline-none focus:border-solid focus:border-b-lavenderIndigo"
+    class="borber-b-pastelPurple w-3/4 border-b-2 border-dashed bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
   />
+  <button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
 </div>
--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import CircledAmount from '$lib/components/CircledAmount.svelte'
   import Search from '$lib/components/Search.svelte'
-  import Status from '$lib/components/Status.svelte'
+  import Tag from '$lib/components/Tag.svelte'
   import { Icon, EllipsisHorizontal, Eye } from 'svelte-hero-icons'
 </script>
 
@@ -9,14 +9,16 @@
   <title>Invoices | The Dollar Holler</title>
 </svelte:head>
 
-<div class="mb-16 flex w-full items-center justify-between">
+<div
+  class="mb-7 flex w-full flex-col-reverse items-start justify-between gap-y-6 md:flex-row md:items-center md:gap-y-4 lg:mb-16"
+>
   <!-- Search field -->
   <Search />
 
   <!-- new invoice button -->
   <div>
     <button
-      class="relative translate-y-0 whitespace-nowrap rounded-lg bg-lavenderIndigo px-10 py-3 font-sansSerif text-xl font-black text-white shadow-colored transition-all hover:-translate-y-2 hover:shadow-coloredHover"
+      class="relative translate-y-0 whitespace-nowrap rounded-lg bg-lavenderIndigo px-5 py-2 font-sansSerif text-base font-black text-white shadow-colored transition-all hover:-translate-y-2 hover:shadow-coloredHover lg:px-10 lg:py-3 lg:text-xl"
       >+ Invoice</button
     >
   </div>
@@ -25,7 +27,7 @@
 <!-- list of invoices -->
 <div>
   <!-- header -->
-  <div class="table-header invoice-table text-daisyBush">
+  <div class="lg:invoice-table table-header hidden text-daisyBush">
     <h3>Status</h3>
     <h3>Due Date</h3>
     <h3>ID</h3>
@@ -36,18 +38,18 @@
   </div>
 
   <!-- invoices -->
-  <div class="invoice-table items-center rounded-lg bg-white py-6 shadow-tableRow">
-    <div><Status label="sent" /></div>
-    <div class="text-lg">1/8/2024</div>
-    <div class="text-lg">12345</div>
-    <div class="text-xl font-bold">Compressed.fm</div>
-    <div class="font-mondo text-lg font-bold">$504.00</div>
-    <div class="center text-lg">
+  <div class="invoice-table invoice-row items-center rounded-lg bg-white py-3 shadow-tableRow lg:py-6">
+    <div class="status"><Tag label="sent" className="ml-auto lg:ml-0" /></div>
+    <div class="due-date textsm lg:text-lg">1/8/2024</div>
+    <div class="invoice-number textsm lg:text-lg">12345</div>
+    <div class="client-name text-base font-bold lg:text-xl">Compressed.fm</div>
+    <div class="font-mondo amount textsm text-right font-bold lg:text-lg">$504.00</div>
+    <div class="lg:center view-button textsm hidden lg:text-lg">
       <button class="text-pastelPurple hover:text-daisyBush">
         <Icon src={Eye} size="24" solid />
       </button>
     </div>
-    <div class="center text-lg">
+    <div class="lg:center more-buton hidden text-lg">
       <a href="#" class="text-pastelPurple hover:text-daisyBush"><Icon src={EllipsisHorizontal} size="24" solid /></a>
     </div>
   </div>
@@ -58,5 +60,46 @@
 <style lang="postcss">
   .table-header h3 {
     @apply text-xl font-black leading-snug;
+  }
+
+  .invoice-row {
+    grid-template-areas:
+      'invoiceNumber invoiceNumber'
+      'clientName amount'
+      'dueDate status';
+  }
+
+  @media (min-width: 1024px) {
+    .invoice-row {
+      grid-template-areas: 'status dueDate invoiceNumber clientName amount viewButton moreButton';
+    }
+  }
+
+  .invoice-row .status {
+    grid-area: status;
+  }
+
+  .invoice-row .due-date {
+    grid-area: dueDate;
+  }
+
+  .invoice-row .client-name {
+    grid-area: clientName;
+  }
+
+  .invoice-row .invoice-number {
+    grid-area: invoiceNumber;
+  }
+
+  .invoice-row .amount {
+    grid-area: amount;
+  }
+
+  .invoice-row .view-button {
+    grid-area: viewButton;
+  }
+
+  .invoice-row .more-buton {
+    grid-area: moreButton;
   }
 </style>
